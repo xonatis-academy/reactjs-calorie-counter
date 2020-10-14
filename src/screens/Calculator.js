1bab14885378602cda89fc156f6824bb41d43d82
michael <nguyen.hoang.michael@gmail.com>
2020-10-14 14:53:52 +0200

    Adding 1 component

---
+++ src/screens/Calculator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Calculator
+            </div>
+        );
+    }
+}
+
+
+export default Calculator;
