2e2cc678be3926af50b4484180667425a0658538
michael <nguyen.hoang.michael@gmail.com>
2020-10-14 12:20:04 +0200

    Adding state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+
+    state = {
+        prenom: 'Joanne'
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <p>
-                        Edit <code>src/App.js</code> and save to reload.
+                        Bienvenue {this.state.prenom}
             </p>
                     <a
                         className="App-link"
