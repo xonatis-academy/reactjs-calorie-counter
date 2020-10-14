122275ff632111c953c386a60b8c8b8ee177c082
michael <nguyen.hoang.michael@gmail.com>
2020-10-14 15:36:49 +0200

    Adding screen swith

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import './App.css';
 import Calculator from './screens/Calculator';
+import Login from './screens/Login';
+import Profile from './screens/Profile';
+import Register from './screens/Register';
 
 class App extends React.Component {
 
+    state = {
+        ecranAAfficher: 'Register'
+    }
+
     render() {
+
+        let ecranElement = null;
+        if (this.state.ecranAAfficher == 'Login') {
+            ecranElement = (
+                <Login />
+            );
+        } else if (this.state.ecranAAfficher == 'Calculator') {
+            ecranElement = (
+                <Calculator />
+            );
+        } else if (this.state.ecranAAfficher == 'Profile') {
+            ecranElement = (
+                <Profile />
+            );
+        } else if (this.state.ecranAAfficher == 'Register') {
+            ecranElement = (
+                <Register />
+            );
+        }
 
         return (
             <div className="App">
                 <header className="App-header">
-                    <Calculator />
+                    {ecranElement}
                 </header>
             </div>
         );
