b9ae36be178fa135a20bb6edfe9b525adb652a99
michael <nguyen.hoang.michael@gmail.com>
2020-10-16 16:14:15 +0200

    Adding bindings to register inputs

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -2,14 +2,37 @@ import React from 'react';
 
 class Register extends React.Component {
 
+    state = {
+        firsname: '',
+        username: '',
+        password: ''
+    }
+
+    changeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    changePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    changeFirstname = (event) => {
+        this.setState({
+            firsname: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 <p>S'enregistrer maintenant</p>
-                <input type="text" placeholder="Prenom ..." /><br />
-                <input type="text" placeholder="Age ..." /><br />
-                <input type="text" placeholder="Email ..." /><br />
-                <input type="text" placeholder="Mot de passe ..." /><br /><br />
+                <input type="text" placeholder="Prenom ..." value={this.state.firsname} onChange={this.changeFirstname} /><br />
+                <input type="text" placeholder="Email ..." value={this.state.username} onChange={this.changeUsername} /><br />
+                <input type="text" placeholder="Mot de passe ..." value={this.state.password} onChange={this.changePassword} /><br /><br />
                 <button onClick={this.props.goToLoginBox}>S'inscrire</button><br />
                 <button onClick={this.props.goToLoginBox}>Retour</button>
             </div>
