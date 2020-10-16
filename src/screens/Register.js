517be93e8cc322bbea1d06753852b31fbc475cf6
michael <nguyen.hoang.michael@gmail.com>
2020-10-16 16:26:39 +0200

    Adding add user in register component

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import ServiceContext from '../ServiceContext';
 
 class Register extends React.Component {
 
+    static contextType = ServiceContext;
+
     state = {
-        firsname: '',
+        firstname: '',
         username: '',
-        password: ''
+        password: '',
+        message: ''
     }
 
     changeUsername = (event) => {
@@ -22,18 +26,30 @@ class Register extends React.Component {
 
     changeFirstname = (event) => {
         this.setState({
-            firsname: event.target.value
+            firstname: event.target.value
         })
+    }
+
+    addUser = () => {
+        let success = this.context.userService.add(this.state.username, this.state.password, this.state.firstname);
+        if (success) {
+            this.props.goToLoginBox();
+        } else {
+            this.setState({
+                message: 'Username déjà pris'
+            })
+        }
     }
 
     render() {
         return (
             <div>
                 <p>S'enregistrer maintenant</p>
-                <input type="text" placeholder="Prenom ..." value={this.state.firsname} onChange={this.changeFirstname} /><br />
+                <div>{this.state.message}</div>
+                <input type="text" placeholder="Prenom ..." value={this.state.firstname} onChange={this.changeFirstname} /><br />
                 <input type="text" placeholder="Email ..." value={this.state.username} onChange={this.changeUsername} /><br />
                 <input type="text" placeholder="Mot de passe ..." value={this.state.password} onChange={this.changePassword} /><br /><br />
-                <button onClick={this.props.goToLoginBox}>S'inscrire</button><br />
+                <button onClick={this.addUser}>S'inscrire</button><br />
                 <button onClick={this.props.goToLoginBox}>Retour</button>
             </div>
         );
