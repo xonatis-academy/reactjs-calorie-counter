b7c7a5301425756d6079bc84440249cae8217939
michael <nguyen.hoang.michael@gmail.com>
2020-10-16 14:58:06 +0200

    Adding input sync

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -2,12 +2,29 @@ import React from 'react';
 
 class Login extends React.Component {
 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    changeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    changePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 Login<br />
-                <input type="text" placeholder="Email ..." /><br />
-                <input type="password" placeholder="Mot de passe ..." /><br />
+                <input type="text" placeholder="Email ..." value={this.state.username} onChange={this.changeUsername} /><br />
+                <input type="password" placeholder="Mot de passe ..." value={this.state.password} onChange={this.changePassword} /><br />
                 <button onClick={this.props.goToProfileBox}>S'identifier</button><br />
                 <button onClick={this.props.goToRegisterBox}>Créer un compte</button>
             </div>
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,11 +1,47 @@
 export default class UserService {
 
-    get() {
+    users = [
+        {
+            firstname: 'Jeanna',
+            username: 'jeanna@top.com',
+            password: 'jeanna'
+        },
+        {
+            firstname: 'Kevin',
+            username: 'kevin@top.com',
+            password: 'kevin'
+        },
+        {
+            firstname: 'Burt',
+            username: 'burt@top.com',
+            password: 'burt'
+        }
+    ]
 
+    // precondition : username : string
+    // postcondition : recuperer l'utilisateur dont le username est égal au username en argment : dictionnaire
+    get(usernameToMatch) {
+
+        for (const a of this.users) {
+            if (a.username == usernameToMatch) {
+                return a;
+            }
+        }
+
+        return null;
     }
     
-    connect() {
-    
+    // precondition : username: string, password: string
+    // postcondition : si le username et le mot de passe sont valides alors True sinon False : boolean
+    connect(usernameToSearch, passwordToSearch) {
+
+        for (const a of this.users) {
+            if (a.username == usernameToSearch && a.password == passwordToSearch) {
+                return true;
+            }
+        }
+
+        return false;
     }
     
     add() {
