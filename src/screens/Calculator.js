8a61bdb798072845ead9422f1fe2319841f404a2
michael <nguyen.hoang.michael@gmail.com>
2020-10-16 17:02:23 +0200

    Adidng ingredient list

--- src/screens/Calculator.js
+++ src/screens/Calculator.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import ServiceContext from '../ServiceContext';
 
 class Calculator extends React.Component {
 
+    static contextType = ServiceContext;
+
     render() {
+        let ingredients = this.context.ingredientService.list();
+        let listeDeBoutonsPourLesIngredients = [];
+        for (const a of ingredients) {
+            let bouton = (
+                <button>{a.name} ({a.calorieParGram})</button>
+            );
+            listeDeBoutonsPourLesIngredients.push(bouton);
+        }
+
         return (
             <div>
-                <button>Oeuf</button>
-                <button>Tomate</button>
-                <button>Courgette</button>
-                <button>Poire</button><br /><br />
+                {listeDeBoutonsPourLesIngredients}<br /><br />
                 Tomate<br />
                 <input type="text" value="123" placeholder="Quantité ..." /><br />
                 <button>Ajouter au plat</button><br />
--- src/services/IngredientService.js
+++ src/services/IngredientService.js
@@ -1,7 +1,28 @@
 export default class IngredientService {
 
-    list() {
+    ingredients = [
+        {
+            name: 'Oeuf',
+            calorieParGram: 23
+        },
+        {
+            name: 'Tomate',
+            calorieParGram: 238
+        },
+        {
+            name: 'Courgette',
+            calorieParGram: 211
+        },
+        {
+            name: 'Poire',
+            calorieParGram: 455
+        }
+    ]
 
+    // precondition : /
+    // postcondition : renvoie tous les ingredients : liste de dictionnaire
+    list() {
+        return this.ingredients;
     }
 
 }
